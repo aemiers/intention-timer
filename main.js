0d3d038eb837aa958d74df2643d6864bdde5f4f5
Anneke Miers <annekemiers.am@gmail.com>
2021-01-07 19:19:05 -0700

    Complete change image onclick

--- main.js
+++ main.js
@@ -8,21 +8,20 @@ var secondInput = document.querySelector('.second-input');
 var startActivityBtn = document.querySelector('.go-button');
 var form = document.querySelector('.activity-form');
 var currentActivity = document.querySelector('.current-activity');
+var taskInput = document.querySelector('.task-input');
+var newActivityInput = document.querySelector('.new-task-input');
+var minuteDisplay = document.querySelector('.min-display');
+var secondDisplay = document.querySelector('.sec-display');
+var timerDisplay = document.querySelector('span');
+var startTimerBtn = document.querySelector('.start-button')
 
 //  ~~~~~~~~~~~~~~~~~ EVENT LISTENERS ~~~~~~~~~~~~~~~~~
-// studyBtn.addEventListener('onclick', changeStudyButtonColor);
-
-// studyBtn.addEventListener('click', function(event) {
-//   changeStudyColor();
-// });
-
-// button, activeClassName, imageClassNameOne, imageClassNameTwo
-// meditateBtn.addEventListener('click', changeImage);
-// exerciseBtn.addEventListener('click', changeImage);
-// startActivityBtn.addEventListener('click', startActivity);
+startActivityBtn.addEventListener('click', startActivity);
+startTimerBtn.addEventListener('click', startTimer);
 
 //  ~~~~~~~~~~~~~~~~~ GLOBAL VARIABLES ~~~~~~~~~~~~~~~~~
-
+var currentActivity;
+var pastActivity;
 //  ~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~
 
 function toggleHidden(elementOne, elementTwo) {
@@ -37,29 +36,6 @@ function addHidden(className) {
 function removeHidden(className) {
   document.querySelector(className).classList.remove("hidden");
 }
-
-// function changeButtonColor(button, activeClassName, imageClassNameOne, imageClassNameTwo) {
-//   event.preventDefault();
-//   if (event.target.id === button) {
-//     event.target.classList.add(activeClassName);
-//     removeHidden(imageClassNameOne);
-//     addHidden(imageClassNameTwo);
-//   }
-// }
-//
-// function changeStudyColor() {
-//   changeButtonColor(studyBtn, "studyBtnActive", ".studyBtnClassOne", ".studyBtnClassTwo");
-// }
-
-
-// function changeStudyButtonColor() {
-//   event.preventDefault();
-//   if (event.target.id === "studyBtn") {
-//     event.target.classList.add("studyBtnActive");
-//     removeHidden(".studyBtnClassTwo");
-//     addHidden(".studyBtnClassOne")
-//   }
-// }
 
 studyBtn.addEventListener('click', function(event) {
   changeStudyColor();
@@ -79,6 +55,7 @@ function changeStudyColor() {
     event.target.classList.add("studyBtnActive");
     addHidden(".studyBtnClassOne");
     removeHidden(".studyBtnClassTwo");
+    currentActivity.category = 'study';
   }
 }
 
@@ -88,6 +65,7 @@ function changeMeditateColor() {
     event.target.classList.add("meditateBtnActive");
     addHidden(".meditateBtnClassOne");
     removeHidden(".meditateBtnClassTwo");
+    currentActivity.category = 'meditate';
   }
 }
 
@@ -97,6 +75,7 @@ function changeExerciseColor() {
     event.target.classList.add("exerciseBtnActive");
     addHidden(".exerciseBtnClassOne");
     removeHidden(".exerciseBtnClassTwo");
+    currentActivity.category = 'exercise';
   }
 }
 
@@ -116,3 +95,97 @@ function changeExerciseColor() {
 // function changeStudyColor() {
 //   changeColor('click', "studyBtn", "studyBtnActive", ".studyBtnClassTwo", ".studyBtnClassOne");
 // }
+
+function addHiddenElement(element) {
+  element.classList.add("hidden");
+}
+
+function removeHiddenElement(element) {
+  element.classList.remove("hidden");
+}
+
+function timerColorizer() {
+  if (studyBtn.classList.includes('studyBtnActive')){
+    startTimerBtn.classList.add('study');
+  } else if (meditateBtn.classList.includes('meditateBtnActive')) {
+     startTimerBtn.classList.add('meditate');
+  } else if (exerciseBtn.classList.includes('exerciseBtnActive')){
+     startTimerBtn.classList.add('exercise');
+  } else {
+    displayError();
+  }
+}
+
+function createNewActivity() {
+  category = ""
+  description = taskInput.value;
+  minutes = minuteInput.value;
+  seconds = secondInput.value;
+  completed = false;
+  currentActivity = new Activity(category, description, minutes, seconds, completed);
+}
+
+function startActivity() {
+  newActivityInput.innerText = taskInput.value;
+  createNewActivity()
+  addHiddenElement(form);
+  removeHiddenElement(currentActivity);
+  showTimer();
+  timerColorizer();
+}
+
+function showTimer() {
+  // var minutes = parseInt(minuteInput.value) < 10 ? "0" + minutes : minutes;
+  // var seconds = parseInt(secondInput.value) < 10 ? "0" + seconds : seconds;
+  timerDisplay.innerHTML = `<span id="time">${minuteInput.value}:${secondInput.value}</span><br />`;
+
+}
+
+function timer(newTime, display) {
+    var timer = newTime, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(minuteInput.value);
+        seconds = parseInt(secondInput.value);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+function startTimer() {
+  event.preventDefault(event);
+    var newTime = (60 * parseInt(minuteInput.value)) + parseInt(secondInput.value);
+        display = document.querySelector('#time');
+    timer(newTime, display);
+};
+
+
+// function timer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+//
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+//
+//         display.textContent = minutes + ":" + seconds;
+//
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+//
+// function startTimer() {
+//   event.preventDefault(event);
+//     var newTime = 60 * 5,
+//         display = document.querySelector('#time');
+//     timer(newTime, display);
+// };
