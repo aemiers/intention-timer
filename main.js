abbefc289b8e745c0c276981903dd62773b14144
Anneke Miers <annekemiers.am@gmail.com>
2021-01-07 18:12:32 -0700

    Add icon, border, and text color to all buttons

--- main.js
+++ main.js
@@ -10,28 +10,109 @@ var form = document.querySelector('.activity-form');
 var currentActivity = document.querySelector('.current-activity');
 
 //  ~~~~~~~~~~~~~~~~~ EVENT LISTENERS ~~~~~~~~~~~~~~~~~
-studyBtn.addEventListener('click', startActivity);
-meditateBtn.addEventListener('click', startActivity);
-exerciseBtn.addEventListener('click', startActivity);
-startActivityBtn.addEventListener('click', startActivity);
+// studyBtn.addEventListener('onclick', changeStudyButtonColor);
+
+// studyBtn.addEventListener('click', function(event) {
+//   changeStudyColor();
+// });
+
+// button, activeClassName, imageClassNameOne, imageClassNameTwo
+// meditateBtn.addEventListener('click', changeImage);
+// exerciseBtn.addEventListener('click', changeImage);
+// startActivityBtn.addEventListener('click', startActivity);
 
 //  ~~~~~~~~~~~~~~~~~ GLOBAL VARIABLES ~~~~~~~~~~~~~~~~~
 
 //  ~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~
 
-function startActivity() {
- form.classList.add('hidden');
- currentActivity.classList.remove('hidden');
+function toggleHidden(elementOne, elementTwo) {
+ elementOne.classList.add('hidden');
+ elementTwo.classList.remove('hidden');
 }
 
-//icon change - if it doesn't have a bug with the onclick in the HTML, leave it. If it does, use below
+function addHidden(className) {
+  document.querySelector(className).classList.add("hidden");
+}
 
-// function changeImage() {
-//     if (document.getElementById("imgClickAndChange").src == "http://www.userinterfaceicons.com/80x80/minimize.png")
-//     {
-//         document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
-//     }
-//     else
-//     {
-//         document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
-//     }
+function removeHidden(className) {
+  document.querySelector(className).classList.remove("hidden");
+}
+
+// function changeButtonColor(button, activeClassName, imageClassNameOne, imageClassNameTwo) {
+//   event.preventDefault();
+//   if (event.target.id === button) {
+//     event.target.classList.add(activeClassName);
+//     removeHidden(imageClassNameOne);
+//     addHidden(imageClassNameTwo);
+//   }
+// }
+//
+// function changeStudyColor() {
+//   changeButtonColor(studyBtn, "studyBtnActive", ".studyBtnClassOne", ".studyBtnClassTwo");
+// }
+
+
+// function changeStudyButtonColor() {
+//   event.preventDefault();
+//   if (event.target.id === "studyBtn") {
+//     event.target.classList.add("studyBtnActive");
+//     removeHidden(".studyBtnClassTwo");
+//     addHidden(".studyBtnClassOne")
+//   }
+// }
+
+studyBtn.addEventListener('click', function(event) {
+  changeStudyColor();
+})
+
+meditateBtn.addEventListener('click', function(event) {
+  changeMeditateColor();
+})
+
+exerciseBtn.addEventListener('click', function(event) {
+  changeExerciseColor();
+})
+
+function changeStudyColor() {
+  event.preventDefault(event);
+  if (event.target.id === "studyBtn") {
+    event.target.classList.add("studyBtnActive");
+    addHidden(".studyBtnClassOne");
+    removeHidden(".studyBtnClassTwo");
+  }
+}
+
+function changeMeditateColor() {
+  event.preventDefault(event);
+  if (event.target.id === "meditateBtn") {
+    event.target.classList.add("meditateBtnActive");
+    addHidden(".meditateBtnClassOne");
+    removeHidden(".meditateBtnClassTwo");
+  }
+}
+
+function changeExerciseColor() {
+  event.preventDefault(event);
+  if (event.target.id === "exerciseBtn") {
+    event.target.classList.add("exerciseBtnActive");
+    addHidden(".exerciseBtnClassOne");
+    removeHidden(".exerciseBtnClassTwo");
+  }
+}
+
+// studyBtn.addEventListener('click', function(event){
+//   changeStudyColor(button, btnActive, btnClassOne, btnClassTwo);
+// });
+//
+// function changeColor(button, btnActive, btnClassOne, btnClassTwo) {
+//   event.preventDefault();
+//   if (event.target.id === button) {
+//     event.target.classList.add(btnActive);
+//     addHidden(btnClassOne);
+//     removeHidden(btnClassTwo);
+//   }
+// }
+//
+// function changeStudyColor() {
+//   changeColor('click', "studyBtn", "studyBtnActive", ".studyBtnClassTwo", ".studyBtnClassOne");
+// }
