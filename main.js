9076a4527176a5900190fedf7cc2f2a5be7d4ad4
Anneke Miers <annekemiers.am@gmail.com>
2021-01-07 20:07:25 -0700

    Fix timer countdown, now starts with input by user and runs properly

--- main.js
+++ main.js
@@ -7,103 +7,83 @@ var minuteInput = document.querySelector('.minute-input');
 var secondInput = document.querySelector('.second-input');
 var startActivityBtn = document.querySelector('.go-button');
 var form = document.querySelector('.activity-form');
-var currentActivity = document.querySelector('.current-activity');
+var currentActivity = document.querySelector('.current-activity-form');
 var taskInput = document.querySelector('.task-input');
 var newActivityInput = document.querySelector('.new-task-input');
 var minuteDisplay = document.querySelector('.min-display');
 var secondDisplay = document.querySelector('.sec-display');
 var timerDisplay = document.querySelector('span');
-var startTimerBtn = document.querySelector('.start-button')
+var startTimerBtn = document.querySelector('.start-button');
 
-//  ~~~~~~~~~~~~~~~~~ EVENT LISTENERS ~~~~~~~~~~~~~~~~~
-startActivityBtn.addEventListener('click', startActivity);
-startTimerBtn.addEventListener('click', startTimer);
-
-//  ~~~~~~~~~~~~~~~~~ GLOBAL VARIABLES ~~~~~~~~~~~~~~~~~
-var currentActivity;
-var pastActivity;
-//  ~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~
-
-function toggleHidden(elementOne, elementTwo) {
- elementOne.classList.add('hidden');
- elementTwo.classList.remove('hidden');
-}
-
-function addHidden(className) {
-  document.querySelector(className).classList.add("hidden");
-}
-
-function removeHidden(className) {
-  document.querySelector(className).classList.remove("hidden");
-}
+// //  ~~~~~~~~~~~~~~~~~ EVENT LISTENERS ~~~~~~~~~~~~~~~~~
 
 studyBtn.addEventListener('click', function(event) {
   changeStudyColor();
 })
-
 meditateBtn.addEventListener('click', function(event) {
   changeMeditateColor();
 })
-
 exerciseBtn.addEventListener('click', function(event) {
   changeExerciseColor();
 })
+startActivityBtn.addEventListener('click', startActivity);
+startTimerBtn.addEventListener('click', startTimer);
 
-function changeStudyColor() {
-  event.preventDefault(event);
-  if (event.target.id === "studyBtn") {
-    event.target.classList.add("studyBtnActive");
-    addHidden(".studyBtnClassOne");
-    removeHidden(".studyBtnClassTwo");
-    currentActivity.category = 'study';
-  }
-}
+// // //  ~~~~~~~~~~~~~~~~~ GLOBAL VARIABLES ~~~~~~~~~~~~~~~~~
+var currentActivity;
+var pastActivity;
 
-function changeMeditateColor() {
-  event.preventDefault(event);
-  if (event.target.id === "meditateBtn") {
-    event.target.classList.add("meditateBtnActive");
-    addHidden(".meditateBtnClassOne");
-    removeHidden(".meditateBtnClassTwo");
-    currentActivity.category = 'meditate';
-  }
-}
-
-function changeExerciseColor() {
-  event.preventDefault(event);
-  if (event.target.id === "exerciseBtn") {
-    event.target.classList.add("exerciseBtnActive");
-    addHidden(".exerciseBtnClassOne");
-    removeHidden(".exerciseBtnClassTwo");
-    currentActivity.category = 'exercise';
-  }
-}
-
-// studyBtn.addEventListener('click', function(event){
-//   changeStudyColor(button, btnActive, btnClassOne, btnClassTwo);
-// });
+// // //  ~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~
+// // function toggleHidden(elementOne, elementTwo) {
+// //  elementOne.classList.add('hidden');
+// //  elementTwo.classList.remove('hidden');
+// // }
+// //
+// // function addHidden(className) {
+// //   document.querySelector(className).classList.add("hidden");
+// // }
+// //
+// // function removeHidden(className) {
+// //   document.querySelector(className).classList.remove("hidden");
+// // }
+// //
+// // function changeStudyColor() {
+// //   event.preventDefault(event);
+// //   if (event.target.id === "studyBtn") {
+// //     event.target.classList.add("studyBtnActive");
+// //     addHidden(".studyBtnClassOne");
+// //     removeHidden(".studyBtnClassTwo");
+// //     // currentActivity.category = 'study';
+// //   }
+// // }
+// //
+// // function changeMeditateColor() {
+// //   event.preventDefault(event);
+// //   if (event.target.id === "meditateBtn") {
+// //     event.target.classList.add("meditateBtnActive");
+// //     addHidden(".meditateBtnClassOne");
+// //     removeHidden(".meditateBtnClassTwo");
+// //     currentActivity.category = 'meditate';
+// //   }
+// // }
+// //
+// // function changeExerciseColor() {
+// //   event.preventDefault(event);
+// //   if (event.target.id === "exerciseBtn") {
+// //     event.target.classList.add("exerciseBtnActive");
+// //     addHidden(".exerciseBtnClassOne");
+// //     removeHidden(".exerciseBtnClassTwo");
+// //     currentActivity.category = 'exercise';
+// //   }
+// // }
 //
-// function changeColor(button, btnActive, btnClassOne, btnClassTwo) {
-//   event.preventDefault();
-//   if (event.target.id === button) {
-//     event.target.classList.add(btnActive);
-//     addHidden(btnClassOne);
-//     removeHidden(btnClassTwo);
-//   }
-// }
-//
-// function changeStudyColor() {
-//   changeColor('click', "studyBtn", "studyBtnActive", ".studyBtnClassTwo", ".studyBtnClassOne");
-// }
-
-function addHiddenElement(element) {
+//  ~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~
+function addHidden(element) {
   element.classList.add("hidden");
 }
-
-function removeHiddenElement(element) {
+function removeHidden(element) {
   element.classList.remove("hidden");
 }
-
 function timerColorizer() {
   if (studyBtn.classList.includes('studyBtnActive')){
     startTimerBtn.classList.add('study');
@@ -115,57 +95,46 @@ function timerColorizer() {
     displayError();
   }
 }
-
 function createNewActivity() {
-  category = ""
+  category =
   description = taskInput.value;
   minutes = minuteInput.value;
   seconds = secondInput.value;
   completed = false;
-  currentActivity = new Activity(category, description, minutes, seconds, completed);
+  activity = new Activity(category, description, minutes, seconds, completed);
 }
-
 function startActivity() {
   newActivityInput.innerText = taskInput.value;
   createNewActivity()
-  addHiddenElement(form);
-  removeHiddenElement(currentActivity);
+  addHidden(form);
+  removeHidden(currentActivity);
   showTimer();
   timerColorizer();
 }
-
 function showTimer() {
   // var minutes = parseInt(minuteInput.value) < 10 ? "0" + minutes : minutes;
   // var seconds = parseInt(secondInput.value) < 10 ? "0" + seconds : seconds;
   timerDisplay.innerHTML = `<span id="time">${minuteInput.value}:${secondInput.value}</span><br />`;
-
 }
-
 function timer(newTime, display) {
     var timer = newTime, minutes, seconds;
     setInterval(function () {
-        minutes = parseInt(minuteInput.value);
-        seconds = parseInt(secondInput.value);
-
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
         display.textContent = minutes + ":" + seconds;
-
         if (--timer < 0) {
             timer = duration;
         }
     }, 1000);
 }
-
 function startTimer() {
   event.preventDefault(event);
     var newTime = (60 * parseInt(minuteInput.value)) + parseInt(secondInput.value);
         display = document.querySelector('#time');
     timer(newTime, display);
 };
-
-
 // function timer(duration, display) {
 //     var timer = duration, minutes, seconds;
 //     setInterval(function () {
